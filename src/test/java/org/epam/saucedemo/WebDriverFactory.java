2449884f4bb39696fc0d826403005890ba151f64
kolyageshko <kolya.geshko@gmail.com>
2025-07-04 09:10:33 +0300

    Rewrite tests to use TestNG, AssertJ, CSS selectors, DataProvider and browser selection via system properties

---
+++ src/test/java/org/epam/saucedemo/WebDriverFactory.java
@@ -0,0 +1,26 @@
+package org.epam.saucedemo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+    public static WebDriver createDriver() {
+        String browser = System.getProperty("browser", "chrome").toLowerCase();
+        switch (browser) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver();
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                return new EdgeDriver();
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+    }
+} 
